64c2f2df03e6e3eec9f13e4000de73afa3a079f2
Frederic Weisbecker <fweisbec@gmail.com>
2011-08-15 01:12:19 +0200

    rcu: Switch to extended quiescent state in userspace from nohz cpuset
    When we switch to adaptive nohz mode and we run in userspace,
    we can still receive IPIs from the RCU core if a grace period
    has been started by another CPU because we need to take part
    of its completion.
    
    However running in userspace is similar to that of running in
    idle because we don't make use of RCU there, thus we can be
    considered as running in RCU extended quiescent state. The
    benefit when running into that mode is that we are not
    anymore disturbed by needless IPIs coming from the RCU core.
    
    To perform this, we just to use the RCU extended quiescent state
    APIs on the following points:
    
    - kernel exit or tick stop in userspace: here we switch to extended
    quiescent state because we run in userspace without the tick.
    
    - kernel entry or tick restart: here we exit the extended quiescent
    state because either we enter the kernel and we may make use of RCU
    read side critical section anytime, or we need the timer tick for some
    reason and that takes care of RCU grace period in a traditional way.
    
    TODO: hook into do_notify_resume() because we may have called
    rcu_enter_nohz() from syscall exit hook, but we might call
    do_notify_resume() right after, which may use RCU.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Anton Blanchard <anton@au1.ibm.com>
    Cc: Avi Kivity <avi@redhat.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Lai Jiangshan <laijs@cn.fujitsu.com>
    Cc: Paul E . McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Paul Menage <menage@google.com>
    Cc: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Cc: Stephen Hemminger <shemminger@vyatta.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tim Pepper <lnxninja@linux.vnet.ibm.com>

--- include/linux/tick.h
+++ include/linux/tick.h
@@ -140,6 +140,7 @@ extern u64 get_cpu_iowait_time_us(int cpu, u64 *last_update_time);
 
 #ifdef CONFIG_CPUSETS_NO_HZ
 DECLARE_PER_CPU(int, task_nohz_mode);
+DECLARE_PER_CPU(int, nohz_task_ext_qs);
 
 extern void tick_nohz_enter_kernel(void);
 extern void tick_nohz_exit_kernel(void);
@@ -148,6 +149,7 @@ extern void tick_nohz_exit_exception(struct pt_regs *regs);
 extern int tick_nohz_adaptive_mode(void);
 extern void tick_nohz_pre_schedule(void);
 extern void tick_nohz_post_schedule(void);
+extern void tick_nohz_cpu_exit_qs(void);
 extern bool tick_nohz_account_tick(void);
 extern void tick_nohz_flush_current_times(bool restart_tick);
 #else /* !CPUSETS_NO_HZ */
--- kernel/time/tick-sched.c
+++ kernel/time/tick-sched.c
@@ -805,6 +805,7 @@ void tick_check_idle(int cpu)
 }
 
 #ifdef CONFIG_CPUSETS_NO_HZ
+DEFINE_PER_CPU(int, nohz_task_ext_qs);
 
 void tick_nohz_exit_kernel(void)
 {
@@ -831,6 +832,9 @@ void tick_nohz_exit_kernel(void)
 	ts->saved_jiffies = jiffies;
 	ts->saved_jiffies_whence = JIFFIES_SAVED_USER;
 
+	__get_cpu_var(nohz_task_ext_qs) = 1;
+	rcu_user_enter();
+
 	local_irq_restore(flags);
 }
 
@@ -847,6 +851,11 @@ void tick_nohz_enter_kernel(void)
 		return;
 	}
 
+	if (__get_cpu_var(nohz_task_ext_qs) == 1) {
+		__get_cpu_var(nohz_task_ext_qs) = 0;
+		rcu_user_exit();
+	}
+
 	ts = &__get_cpu_var(tick_cpu_sched);
 
 	WARN_ON_ONCE(ts->saved_jiffies_whence == JIFFIES_SAVED_SYS);
@@ -860,6 +869,14 @@ void tick_nohz_enter_kernel(void)
 	ts->saved_jiffies_whence = JIFFIES_SAVED_SYS;
 
 	local_irq_restore(flags);
+}
+
+void tick_nohz_cpu_exit_qs(void)
+{
+	if (__get_cpu_var(nohz_task_ext_qs)) {
+		rcu_user_exit_irq();
+		__get_cpu_var(nohz_task_ext_qs) = 0;
+	}
 }
 
 void tick_nohz_enter_exception(struct pt_regs *regs)
@@ -924,6 +941,8 @@ static void tick_nohz_cpuset_stop_tick(int user)
 		if (user) {
 			ts->saved_jiffies_whence = JIFFIES_SAVED_USER;
 			ts->saved_jiffies = jiffies;
+			__get_cpu_var(nohz_task_ext_qs) = 1;
+			rcu_user_enter_irq();
 		} else if (!current->mm) {
 			ts->saved_jiffies_whence = JIFFIES_SAVED_SYS;
 			ts->saved_jiffies = jiffies;
@@ -955,6 +974,7 @@ static void cpuset_nohz_restart_tick(void)
 	__get_cpu_var(task_nohz_mode) = 0;
 	tick_nohz_restart_sched_tick();
 	clear_thread_flag(TIF_NOHZ);
+	tick_nohz_cpu_exit_qs();
 }
 
 void cpuset_update_nohz(void)
@@ -979,6 +999,7 @@ void tick_nohz_pre_schedule(void)
 	if (tick_nohz_adaptive_mode()) {
 		tick_nohz_flush_current_times(true);
 		clear_thread_flag(TIF_NOHZ);
+		/* FIXME: warn if we are in RCU idle mode */
 	}
 }
 
