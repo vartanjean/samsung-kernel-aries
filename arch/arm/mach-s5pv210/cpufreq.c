004105b1145390e11b44c360b11eb7a2a39e0158
Ezekeel <notezekeel@googlemail.com>
2012-01-22 22:23:47 +0100

    When the GPU is active the minimum frequency is adjusted to the second lowest frequency taking into account the current OC value.

--- arch/arm/mach-s5pv210/cpufreq.c
+++ arch/arm/mach-s5pv210/cpufreq.c
@@ -737,6 +737,12 @@ void liveoc_update(unsigned int oc_value)
     return;
 }
 EXPORT_SYMBOL(liveoc_update);
+
+unsigned long get_gpuminfreq(void)
+{
+    return s5pv210_freq_table[L3].frequency;
+}
+EXPORT_SYMBOL(get_gpuminfreq);
 #endif
 
 #ifdef CONFIG_CUSTOM_VOLTAGE
