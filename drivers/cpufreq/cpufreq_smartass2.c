stevegaron/SG-NS-ICS

Fix smartass workqueue. create_rt_workqueue is a deprecated method. Changed it to create_singlethread_workqueue.

drivers/cpufreq/cpufreq_smartass2.c
@@ -836,7 +836,7 @@ static int __init cpufreq_smartass_init(void)
 	}
 
 	// Scale up is high priority
-	up_wq = create_rt_workqueue("ksmartass_up");
+	up_wq = create_singlethread_workqueue("ksmartass_up");
 	down_wq = create_workqueue("ksmartass_down");
 	if (!up_wq || !down_wq)
 		return -ENOMEM;

vartanjean/samsung-kernel-aries

drivers/cpufreq/cpufreq_smartass2.c
@@ -836,9 +836,9 @@ static int __init cpufreq_smartass2_init(void)
 	}
 
 	// Scale up is high priority
-	up_wq = create_workqueue("ksmartass2_up");
+	up_wq = create_singlethread_workqueue("ksmartass2_up");
 	down_wq = create_workqueue("ksmartass2_down");
-	if (!up_wq || !down_wq)
+ 	if (!up_wq || !down_wq)
 		return -ENOMEM;
 
 	INIT_WORK(&freq_scale_work, cpufreq_smartass2_freq_change_time_work);