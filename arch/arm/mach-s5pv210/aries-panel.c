32b6de742be80f85b50d28cea41961e9965f3810
Dominik Baronelli <dominik.baronelli@gmail.com>
2012-02-28 21:05:06 +0100

    6500k color

--- arch/arm/mach-s5pv210/aries-panel.c
+++ arch/arm/mach-s5pv210/aries-panel.c
@@ -357,20 +357,21 @@ struct s5p_panel_data aries_panel_data = {
 		0x0b8,
 		0x0fc,
 	},
-	.color_adj = {
+
+//	.color_adj = {
 		/* Convert from 8500K to D65, assuming:
 		 * Rx 0.66950, Ry 0.33100
 		 * Gx 0.18800, Gy 0.74350
 		 * Bx 0.14142, By 0.04258
 		 */
-		.mult = {
+/*		.mult = {
 			2318372099U,
 			2117262806U,
 			2082349739U,
 		},
 		.rshift = 31,
 	},
-
+*/
 	.gamma_adj_points = &gamma_adj_points,
 	.gamma_table = gamma_table,
 	.gamma_table_size = ARRAY_SIZE(gamma_table),
--- bln.sh
+++ bln.sh
@@ -1,26 +1,40 @@
-#!/bin/sh
-build="Devil_1.1.5"
 ######## Building BLN Kernel ##########################################################
 
 	light="BLN"
 
-#	rm "arch/arm/mach-s5pv210/aries-touchkey-led.c"
-#	cp "arch/arm/mach-s5pv210/aries-touchkey-led.bln" "arch/arm/mach-s5pv210/aries-touchkey-led.c"
-
-#	rm "drivers/input/keyboard/cypress-touchkey.c"
-#	cp "drivers/input/keyboard/cypress-touchkey.bln" "drivers/input/keyboard/cypress-touchkey.c"
-
-	sed -i 's/^.*BLD.*$//' arch/arm/configs/aries_galaxysmtd_defconfig
-	echo '# CONFIG_BLD is not set' >> arch/arm/configs/aries_galaxysmtd_defconfig
-
-#	sed -i 's/^.*KEYPAD_CYPRESS_TOUCH_BLN.*$//' arch/arm/configs/aries_galaxysmtd_defconfig
-#	echo 'CONFIG_KEYPAD_CYPRESS_TOUCH_BLN=y' >> arch/arm/configs/aries_galaxysmtd_defconfig
-
-	sed -e "/^ *$/d" arch/arm/configs/aries_galaxysmtd_defconfig > arch/arm/configs/aries_galaxysmtd_neu
-	mv arch/arm/configs/aries_galaxysmtd_neu arch/arm/configs/aries_galaxysmtd_defconfig
-
 ############### BFS kernel #########################
 scheduler="BFS"
+echo "building for galaxy s"
+make aries_galaxysmtd_defconfig
+echo "building BFS kernel with voodoo color"
+sed -i 's/^.*SCHED_BFS.*$//' .config
+echo 'CONFIG_SCHED_BFS=y' >> .config
+
+sed -i 's/^.*FB_VOODOO.*$//' .config
+echo 'CONFIG_FB_VOODOO=y
+CONFIG_FB_VOODOO_DEBUG_LOG=n' >> .config
+color="VC"
+version="$build"_"$scheduler"_"$light"_"$color"
+sed "/Devil/c\ \" ("$version")\"" init/version.c > init/version.neu
+mv init/version.c init/version.backup
+mv init/version.neu init/version.c
+echo "building kernel"
+make -j4
+
+echo "creating boot.img"
+if [ ! -d "release/$light/BFS/voodoo" ];
+then
+	mkdir -p release/$light/BFS/voodoo
+fi
+release/build-scripts/mkshbootimg.py release/$light/BFS/voodoo/boot.img arch/arm/boot/zImage release/ramdisks/galaxys_ramdisk/ramdisk.img release/ramdisks/galaxys_ramdisk/ramdisk-recovery.img
+echo "..."
+echo "boot.img ready"
+
+echo "launching packaging script"
+
+. ./packaging.inc
+release "${version}"
+#######################################################################################################################
 
 echo "building for galaxy s"
 make aries_galaxysmtd_defconfig
@@ -40,8 +54,11 @@ echo "building kernel"
 make -j4
 
 echo "creating boot.img"
-
-release/build-scripts/mkshbootimg.py release/boot.img arch/arm/boot/zImage release/ramdisks/galaxys_ramdisk/ramdisk.img release/ramdisks/galaxys_ramdisk/ramdisk-recovery.img
+if [ ! -d "release/$light/BFS/no_voodoo" ];
+then
+	mkdir -p release/$light/BFS/no_voodoo
+fi
+release/build-scripts/mkshbootimg.py release/$light/BFS/no_voodoo/boot.img arch/arm/boot/zImage release/ramdisks/galaxys_ramdisk/ramdisk.img release/ramdisks/galaxys_ramdisk/ramdisk-recovery.img
 echo "..."
 echo "boot.img ready"
 
@@ -50,10 +67,46 @@ echo "launching packaging script"
 . ./packaging.inc
 release "${version}"
 
-rm /release/boot.img
-
 ########################### CFS kernel #############################################
 scheduler="CFS"
+make aries_galaxysmtd_defconfig
+echo "building CFS kernel with voodoo color"
+sed -i 's/^.*SCHED_BFS.*$//' .config
+echo 'CONFIG_SCHED_BFS=n
+CONFIG_CGROUP_CPUACCT=y
+CONFIG_CGROUP_SCHED=y
+CONFIG_FAIR_GROUP_SCHED=y
+CONFIG_RT_GROUP_SCHED=y
+CONFIG_SCHED_AUTOGROUP=y
+# CONFIG_RCU_TORTURE_TEST is not set' >> .config
+
+sed -i 's/^.*FB_VOODOO.*$//' .config
+echo 'CONFIG_FB_VOODOO=y
+CONFIG_FB_VOODOO_DEBUG_LOG=n' >> .config
+color="VC"
+version="$build"_"$scheduler"_"$light"_"$color"
+sed "/Devil/c\ \" ("$version")\"" init/version.c > init/version.neu
+mv init/version.c init/version.backup
+mv init/version.neu init/version.c
+echo "building kernel"
+ make -j4
+
+echo "creating boot.img"
+if [ ! -d "release/$light/CFS/voodoo" ];
+then
+	mkdir -p release/$light/CFS/voodoo
+fi
+release/build-scripts/mkshbootimg.py release/$light/CFS/voodoo/boot.img arch/arm/boot/zImage release/ramdisks/galaxys_ramdisk/ramdisk.img release/ramdisks/galaxys_ramdisk/ramdisk-recovery.img
+echo "..."
+echo "boot.img ready"
+
+echo "launching packaging script"
+
+. ./packaging.inc
+release "${version}"
+
+#######################################################################################################################
+
 echo "building for galaxy s"
 make aries_galaxysmtd_defconfig
 echo "building CFS kernel without voodoo color"
@@ -78,8 +131,11 @@ echo "building kernel"
 make -j4
 
 echo "creating boot.img"
-
-release/build-scripts/mkshbootimg.py release/boot.img arch/arm/boot/zImage release/ramdisks/galaxys_ramdisk/ramdisk.img release/ramdisks/galaxys_ramdisk/ramdisk-recovery.img
+if [ ! -d "release/$light/CFS/no_voodoo" ];
+then
+	mkdir -p release/$light/CFS/no_voodoo
+fi
+release/build-scripts/mkshbootimg.py release/$light/CFS/no_voodoo/boot.img arch/arm/boot/zImage release/ramdisks/galaxys_ramdisk/ramdisk.img release/ramdisks/galaxys_ramdisk/ramdisk-recovery.img
 echo "..."
 echo "boot.img ready"
 
@@ -87,7 +143,5 @@ echo "launching packaging script"
 
 . ./packaging.inc
 release "${version}"
-
-rm boot.img
 
 echo "all done!"
