77e6e54c748d929474f0ecdbc28caa7fc4f43a92
Dominik Baronelli <dominik.baronelli@gmail.com>
2012-03-01 23:18:40 +0100

    Revert "additional changes to: sched: fix nohz idle load balancer issues"
    This reverts commit 045f01537038e9bb81eaf1244d0af9e2cc7509d2.

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -3601,6 +3601,21 @@ out_unlock:
 
 #ifdef CONFIG_NO_HZ
 
+static DEFINE_PER_CPU(struct call_single_data, remote_sched_softirq_cb);
+
+static void trigger_sched_softirq(void *data)
+{
+	raise_softirq_irqoff(SCHED_SOFTIRQ);
+}
+
+static inline void init_sched_softirq_csd(struct call_single_data *csd)
+{
+	csd->func = trigger_sched_softirq;
+	csd->info = NULL;
+	csd->flags = 0;
+	csd->priv = 0;
+}
+
 /*
  * idle load balancing details
  * - One of the idle CPUs nominates itself as idle load_balancer, while
@@ -3771,9 +3786,11 @@ static void nohz_balancer_kick(int cpu)
 	}
 
 	if (!cpu_rq(ilb_cpu)->nohz_balance_kick) {
+		struct call_single_data *cp;
 
 		cpu_rq(ilb_cpu)->nohz_balance_kick = 1;
-	kick_process(idle_task(ilb_cpu));
+		cp = &per_cpu(remote_sched_softirq_cb, cpu);
+		__smp_call_function_single(ilb_cpu, cp, 0);
 	}
 	return;
 }
@@ -3824,6 +3841,11 @@ void select_nohz_load_balancer(int stop_tick)
 		}
 
 		cpumask_set_cpu(cpu, nohz.idle_cpus_mask);
+
+		if (atomic_read(&nohz.first_pick_cpu) == cpu)
+			atomic_cmpxchg(&nohz.first_pick_cpu, cpu, nr_cpu_ids);
+		if (atomic_read(&nohz.second_pick_cpu) == cpu)
+			atomic_cmpxchg(&nohz.second_pick_cpu, cpu, nr_cpu_ids);
 
 		if (atomic_read(&nohz.load_balancer) >= nr_cpu_ids) {
 			int new_ilb;
@@ -3981,10 +4003,8 @@ static void nohz_idle_balance(int this_cpu, enum cpu_idle_type idle)
 		 * work being done for other cpus. Next load
 		 * balancing owner will pick it up.
 		 */
-		if (need_resched()) {
-		nohz_balancer_kick(this_cpu);
-		break;
-		}
+		if (need_resched())
+			break;
 
 		raw_spin_lock_irq(&this_rq->lock);
 		update_rq_clock(this_rq);
@@ -4022,7 +4042,7 @@ static inline int nohz_kick_needed(struct rq *rq, int cpu)
 	if (time_before(now, nohz.next_balance))
 		return 0;
 
-	if (idle_cpu(cpu))
+	if (rq->idle_at_tick)
 		return 0;
 
 	first_pick_cpu = atomic_read(&nohz.first_pick_cpu);
@@ -4049,23 +4069,9 @@ static inline int nohz_kick_needed(struct rq *rq, int cpu)
 	}
 	return 0;
 }
-
-/*
- * Reset first_pick_cpu or second_pick_cpu identifier in case
- * corresponding cpu is going idle.
- */
-static void reset_first_second_pick_cpu(int cpu)
-{
-  if (atomic_read(&nohz.first_pick_cpu) == cpu)
-   atomic_cmpxchg(&nohz.first_pick_cpu, cpu, nr_cpu_ids);
-  if (atomic_read(&nohz.second_pick_cpu) == cpu)
-    atomic_cmpxchg(&nohz.second_pick_cpu, cpu, nr_cpu_ids);
-}
-
-#else  /* CONFIG_NO_HZ */
+#else
 static void nohz_idle_balance(int this_cpu, enum cpu_idle_type idle) { }
-
-#endif  /* CONFIG_NO_HZ */
+#endif
 
 /*
  * run_rebalance_domains is triggered when needed from the scheduler tick.
@@ -4075,7 +4081,7 @@ static void run_rebalance_domains(struct softirq_action *h)
 {
 	int this_cpu = smp_processor_id();
 	struct rq *this_rq = cpu_rq(this_cpu);
-	enum cpu_idle_type idle = this_rq->idle_balance ?
+	enum cpu_idle_type idle = this_rq->idle_at_tick ?
 						CPU_IDLE : CPU_NOT_IDLE;
 
 	rebalance_domains(this_cpu, idle);
