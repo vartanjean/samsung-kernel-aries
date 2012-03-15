504e93b4e82dfb6f22a14383a7a77e30932b5ec0
Dominik Baronelli <dominik.baronelli@gmail.com>
2012-03-15 11:19:40 +0100

    added bln9 + building for cappy

--- mine.sh
+++ mine.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 rm arch/arm/boot/zImage
-build="Devil2_0.32"
+
+handy="cappy"
+
+build="Devil2_0.33"_"$handy"
 
 scheduler="BFS"
 
-color="CMC"
+color="VC"
 
-light="LED"
+light="BLN"
 
 version="$build"_"$scheduler"_"$light"_"$color"
 export KBUILD_BUILD_VERSION="$build"_"$scheduler"_"$color"
@@ -14,12 +17,30 @@ sed "/Devil/c\ \" ("$version")\"" init/version.c > init/version.neu
 mv init/version.c init/version.backup
 mv init/version.neu init/version.c
 echo "building kernel"
+
+if [ "$handy" = "i9000"  ] 
+then
 make aries_galaxysmtd_defconfig
+fi
+
+if [ "$handy" = "cappy"  ] 
+then
+make aries_captivatemtd_defconfig
+fi
+
 make -j4
 
 echo "creating boot.img"
-
+if [ "$handy" = "i9000"  ] 
+then
 release/build-scripts/mkshbootimg.py release/boot.img arch/arm/boot/zImage release/ramdisks/galaxys_ramdisk/ramdisk.img release/ramdisks/galaxys_ramdisk/ramdisk-recovery.img
+fi
+
+if [ "$handy" = "cappy"  ] 
+then
+release/build-scripts/mkshbootimg.py release/boot.img arch/arm/boot/zImage release/ramdisks/captivate_ramdisk/ramdisk.img release/ramdisks/captivate_ramdisk/ramdisk-recovery.img
+fi
+
 echo "..."
 echo "boot.img ready"
 rm arch/arm/boot/zImage
