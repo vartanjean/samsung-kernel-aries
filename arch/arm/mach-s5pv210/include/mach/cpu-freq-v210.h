a8e2ddcf74024638a3291934e6f9ef58b106ed58
Dominik Baronelli <dominik.baronelli@gmail.com>
2012-03-24 23:42:07 +0100

    600 mhz again, still not working

--- arch/arm/mach-s5pv210/include/mach/cpu-freq-v210.h
+++ arch/arm/mach-s5pv210/include/mach/cpu-freq-v210.h
@@ -15,7 +15,7 @@
 #include <linux/cpufreq.h>
 
 enum perf_level {
-	L0, L1, L2, L3, L4, L5, L6, L7, L8, MAX_PERF_LEVEL = L8,
+	L0, L1, L2, L3, L4, L5, L6, L7, L8, L9, MAX_PERF_LEVEL = L9,
 };
 
 /* For cpu-freq driver */
