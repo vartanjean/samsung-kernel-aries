6cbaf7320048021b40d5c8098721176ed4b5cc99
Dominik Baronelli <dominik.baronelli@gmail.com>
2012-04-10 20:21:37 +0200

    added sysfs to switch wifi power mode

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -309,6 +309,13 @@ uint dhd_console_ms = 0;
 module_param(dhd_console_ms, uint, 0644);
 #endif /* defined(DHD_DEBUG) */
 
+/* Controls Status off wiffi Power on SleepMode
+   sys/module/bcmdhd/parameters/uiFastWifi      */
+#if defined(CONFIG_HAS_EARLYSUSPEND)
+uint uiFastWifi = 0;
+module_param(uiFastWifi, uint, 0664);
+#endif /* defined(CONFIG_HAS_EARLYSUSPEND) */
+
 /* ARP offload agent mode : Enable ARP Host Auto-Reply and ARP Peer Auto-Reply */
 uint dhd_arp_mode = 0xb;
 module_param(dhd_arp_mode, uint, 0);
@@ -523,15 +530,19 @@ static void dhd_set_packet_filter(int value, dhd_pub_t *dhd)
 #if defined(CONFIG_HAS_EARLYSUSPEND)
 static int dhd_set_suspend(int value, dhd_pub_t *dhd)
 {
-#ifdef CONFIG_BCMDHD_PMFAST
-	int power_mode = PM_FAST;
-#else
+//#ifdef CONFIG_BCMDHD_PMFAST
+//	int power_mode = PM_FAST;
+//#else
 	int power_mode = PM_MAX;
-#endif
+//#endif
 	/* wl_pkt_filter_enable_t	enable_parm; */
 	char iovbuf[32];
 	int bcn_li_dtim = 3;
 	uint roamvar = 1;
+
+  /* Don't allow low power is uiFastWifi is set */
+  if (uiFastWifi == 1)
+          power_mode = PM_FAST;
 
 	DHD_TRACE(("%s: enter, value = %d in_suspend=%d\n",
 		__FUNCTION__, value, dhd->in_suspend));
