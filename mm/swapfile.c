794839ead7f922a2d195f2b6053bc38953ad4242
Dominik Baronelli <dominik.baronelli@gmail.com>
2012-05-18 18:41:06 +0200

    finishing frontswap

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -1639,8 +1639,9 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 //	err = try_to_unuse(type);
 //	test_set_oom_score_adj(oom_score_adj);
 //
-	err = try_to_unuse(type);
+	err = try_to_unuse(type, false, 0); /* force all pages to be unused */
 	compare_swap_oom_score_adj(OOM_SCORE_ADJ_MAX, oom_score_adj);
+
 
 	if (err) {
 		/*
