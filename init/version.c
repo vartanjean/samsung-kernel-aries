cf06536c786b18707c42712dd23ba6bacc82f9ee
Dominik Baronelli <dominik.baronelli@gmail.com>
2012-06-09 14:00:00 +0200

    cleaning

--- init/version.c
+++ init/version.c
@@ -44,5 +44,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
- " (Devil3_0.70_fascinate_CFS_BLN_VC)"
+ " (Devil3_0.71_vibrant_CFS_BLN_VC)"
 	" (" LINUX_COMPILER ") %s\n";
