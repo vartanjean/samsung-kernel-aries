d953aa02295672d7145163d1052f445035e92def
Dominik Baronelli <dominik.baronelli@gmail.com>
2012-07-08 23:56:59 +0200

    compiling test kernel 0.99 for all devices

--- usr/galaxysmtd_initramfs/ics_init/sbin/boot-patch.sh
+++ usr/galaxysmtd_initramfs/ics_init/sbin/boot-patch.sh
@@ -129,10 +129,10 @@ swap_use=`cat /data/local/devil/swap_use`
 	echo $RAMSIZE > /data/local/devil/zram_size
 	fi
 
-	if $BB [ "$RAMSIZE" -eq 50 ];then echo "Zram: found vaild RAMSIZE: <$RAMSIZE mb>" 
-	elif $BB [ "$RAMSIZE" -eq 75 ];then echo "Zram: found vaild RAMSIZE: <$RAMSIZE mb>" 
-	elif $BB [ "$RAMSIZE" -eq 100 ];then echo "Zram: found vaild RAMSIZE: <$RAMSIZE mb>" 
-	elif $BB [ "$RAMSIZE" -eq 150 ];then echo "Zram: found vaild RAMSIZE: <$RAMSIZE mb>" 
+	if $BB [ "$RAMSIZE" -eq 50 ];then echo "Zram: found valid RAMSIZE: <$RAMSIZE mb>" 
+	elif $BB [ "$RAMSIZE" -eq 75 ];then echo "Zram: found valid RAMSIZE: <$RAMSIZE mb>" 
+	elif $BB [ "$RAMSIZE" -eq 100 ];then echo "Zram: found valid RAMSIZE: <$RAMSIZE mb>" 
+	elif $BB [ "$RAMSIZE" -eq 150 ];then echo "Zram: found valid RAMSIZE: <$RAMSIZE mb>" 
 	else RAMSIZE=75
 	echo "Zram: set RAMSIZE to: <$RAMSIZE mb>" 
 	fi
@@ -165,10 +165,10 @@ echo; echo "profile"
 	profile=`cat /data/local/devil/profile`
 	echo "profile: found: <$profile>";
 		if [ "$profile" -eq 1 ]; then
-    			echo "profile: found vaild governor profile: <smooth>";
+    			echo "profile: found valid governor profile: <smooth>";
       			echo 1 > /sys/class/misc/devil_tweaks/governors_profile;
 		elif [ "$profile" -eq 2 ]; then
-    			echo "profile: found vaild governor profile: <powersave>";
+    			echo "profile: found valid governor profile: <powersave>";
       			echo 2 > /sys/class/misc/devil_tweaks/governors_profile;
 		else
     			echo "profile: setting governor profile: <normal>";
@@ -189,15 +189,15 @@ if [ -e "/data/local/devil/user_min_max_enable" ];then
    	if [ -e "/data/local/devil/screen_off_max" ];then
 		echo; echo "set cpu max freq while screen off"
 		screen_off_max=`cat /data/local/devil/screen_off_max`
-		if $BB [ "$screen_off_max" -eq 1400000 ];then echo "CPU: found vaild screen_off_max: <$screen_off_max>" 
-			elif $BB [ "$screen_off_max" -eq 1300000 ];then echo "CPU: found vaild screen_off_max: <$screen_off_max>"
-			elif $BB [ "$screen_off_max" -eq 1200000 ];then echo "CPU: found vaild screen_off_max: <$screen_off_max>" 
-			elif $BB [ "$screen_off_max" -eq 1000000 ];then echo "CPU: found vaild screen_off_max: <$screen_off_max>"
-			elif $BB [ "$screen_off_max" -eq 800000 ];then echo "CPU: found vaild screen_off_max: <$screen_off_max>" 
-			elif $BB [ "$screen_off_max" -eq 400000 ];then echo "CPU: found vaild screen_off_max: <$screen_off_max>"
+		if $BB [ "$screen_off_max" -eq 1400000 ];then echo "CPU: found valid screen_off_max: <$screen_off_max>" 
+			elif $BB [ "$screen_off_max" -eq 1300000 ];then echo "CPU: found valid screen_off_max: <$screen_off_max>"
+			elif $BB [ "$screen_off_max" -eq 1200000 ];then echo "CPU: found valid screen_off_max: <$screen_off_max>" 
+			elif $BB [ "$screen_off_max" -eq 1000000 ];then echo "CPU: found valid screen_off_max: <$screen_off_max>"
+			elif $BB [ "$screen_off_max" -eq 800000 ];then echo "CPU: found valid screen_off_max: <$screen_off_max>" 
+			elif $BB [ "$screen_off_max" -eq 400000 ];then echo "CPU: found valid screen_off_max: <$screen_off_max>"
 
    			else
-			echo "CPU: did not find vaild screen_off_max, setting 1000 Mhz as default"
+			echo "CPU: did not find valid screen_off_max, setting 1000 Mhz as default"
 			screen_off_max=1000000
 		fi
 		echo $screen_off_max > /sys/class/misc/devil_idle/user_max
@@ -211,12 +211,12 @@ if [ -e "/data/local/devil/user_min_max_enable" ];then
 	if [ -e "/data/local/devil/screen_off_min" ];then
 		echo; echo "set cpu min freq while screen off"
 		screen_off_min=`cat /data/local/devil/screen_off_min`
-		if $BB [ "$screen_off_min" -eq 100000 ];then echo "CPU: found vaild screen_off_min: <$screen_off_min>"  
-			elif $BB [ "$screen_off_min" -eq 200000 ];then echo "CPU: found vaild screen_off_min: <$screen_off_min>" 
-			elif $BB [ "$screen_off_min" -eq 400000 ];then echo "CPU: found vaild screen_off_min: <$screen_off_min>" 
-			elif $BB [ "$screen_off_min" -eq 800000 ];then echo "CPU: found vaild screen_off_min: <$screen_off_min>" 
+		if $BB [ "$screen_off_min" -eq 100000 ];then echo "CPU: found valid screen_off_min: <$screen_off_min>"  
+			elif $BB [ "$screen_off_min" -eq 200000 ];then echo "CPU: found valid screen_off_min: <$screen_off_min>" 
+			elif $BB [ "$screen_off_min" -eq 400000 ];then echo "CPU: found valid screen_off_min: <$screen_off_min>" 
+			elif $BB [ "$screen_off_min" -eq 800000 ];then echo "CPU: found valid screen_off_min: <$screen_off_min>" 
 		else
-			echo "CPU: did not find vaild screen_off_min, setting 100 Mhz as default"
+			echo "CPU: did not find valid screen_off_min, setting 100 Mhz as default"
 			screen_off_min=100000
 			echo $screen_off_min >/data/local/devil/screen_off_min
 		fi
@@ -240,14 +240,14 @@ echo; echo "fsync"
 if [ -e "/data/local/devil/fsync" ];then
 	fsync=`cat /data/local/devil/fsync`
 	if [ "$fsync" -eq 0 ] || [ "$fsync" -eq 1 ];then
-    		echo "fsync: found vaild fsync mode: <$fsync>"
+    		echo "fsync: found valid fsync mode: <$fsync>"
     		echo $fsync > /sys/devices/virtual/misc/fsynccontrol/fsync_enabled
 	else
-		echo "fsync: did not find vaild fsync mode: setting default"
+		echo "fsync: did not find valid fsync mode: setting default"
 		echo 1 > /sys/devices/virtual/misc/fsynccontrol/fsync_enabled
 	fi
 else
-	echo "fsync: did not find vaild fsync mode: setting default"
+	echo "fsync: did not find valid fsync mode: setting default"
 	echo 1 > /data/local/devil/fsync
 	echo 1 > /sys/devices/virtual/misc/fsynccontrol/fsync_enabled
 fi
@@ -255,16 +255,17 @@ fi
 # set deep_idle
 echo; echo "deep_idle"
 if [ -e "/data/local/devil/deep_idle" ];then
-	fsync=`cat /data/local/devil/deep_idle`
+	deep_idle=`cat /data/local/devil/deep_idle`
 	if [ "$deep_idle" -eq 0 ] || [ "$deep_idle" -eq 1 ];then
-    		echo "deep_idle: found vaild deep_idle mode: <$deep_idle>"
+    		echo "deep_idle: found valid deep_idle mode: <$deep_idle>"
     		echo $deep_idle > /sys/devices/virtual/misc/deepidle/enabled
 	else
-		echo "deep_idle: did not find vaild deep_idle mode: setting disabled"
+		echo "deep_idle: did not find valid deep_idle mode: setting disabled"
 		echo 0 > /sys/devices/virtual/misc/deepidle/enabled
 	fi
 else
-	echo "deep_idle: did not find vaild deep_idle mode: setting disabled"
+	echo "deep_idle: did not find valid deep_idle mode: setting disabled"
+	deep_idle=0
 	echo 0 > /data/local/devil/deep_idle
 	echo 0 > /sys/devices/virtual/misc/deepidle/enabled
 fi
@@ -273,16 +274,16 @@ fi
 echo; echo "deep_idle_stats"
 if [ "$deep_idle" -eq 1 ]; then
    if [ -e "/data/local/devil/deep_idle_stats" ];then
-	fsync=`cat /data/local/devil/deep_idle_stats`
+	deep_idle_stats=`cat /data/local/devil/deep_idle_stats`
 	if [ "$deep_idle_stats" -eq 0 ] || [ "$deep_idle_stats" -eq 1 ];then
-    		echo "deep_idle_stats: found vaild deep_idle_stats mode: <$deep_idle_stats>"
+    		echo "deep_idle_stats: found valid deep_idle_stats mode: <$deep_idle_stats>"
     		echo $deep_idle_stats > /sys/devices/virtual/misc/deepidle/stats_enabled
 	else
-		echo "deep_idle_stats: did not find vaild deep_idle_stats mode: setting disabled"
+		echo "deep_idle_stats: did not find valid deep_idle_stats mode: setting disabled"
 		echo 0 > /sys/devices/virtual/misc/deepidle/stats_enabled
 	fi
     else
-	echo "deep_idle_stats: did not find vaild deep_idle_stats mode: setting disabled"
+	echo "deep_idle_stats: did not find valid deep_idle_stats mode: setting disabled"
 	echo 0 > /data/local/devil/deep_idle_stats
 	echo 0 > /sys/devices/virtual/misc/deepidle/stats_enabled
    fi
@@ -293,16 +294,56 @@ echo; echo "uksm"
 if [ -e "/data/local/devil/uksm" ];then
 	uksm=`cat /data/local/devil/uksm`
 	if [ "$uksm" -eq 1 ]; then
-    		echo "uksm: found vaild uksm value: <enabled>";
+    		echo "uksm: found valid uksm value: <enabled>";
       		echo 1 > /sys/kernel/mm/uksm/run;
 	else
-    		echo "uksm: found vaild uksm value: <disabled>";
+    		echo "uksm: found valid uksm value: <disabled>";
       		echo 0 > /sys/kernel/mm/uksm/run;
 	fi
 else
     	echo "uksm not found: doing nothing";
       	echo 0 > /sys/kernel/mm/uksm/run;
     	echo 0 > /data/local/devil/uksm;
+fi
+
+
+# set touchwake
+echo; echo "touchwake"
+if [ -e "/data/local/devil/touchwake" ];then
+	touchwake=`cat /data/local/devil/touchwake`
+	if [ "$touchwake" -eq 0 ] || [ "$touchwake" -eq 1 ];then
+    		echo "touchwake: found valid touchwake mode: <$touchwake>"
+    		echo $touchwake > /sys/devices/virtual/misc/touchwake/enabled
+	else
+		echo "touchwake: did not find valid touchwake mode: setting disabled"
+		echo 0 > /sys/devices/virtual/misc/touchwake/enabled
+	fi
+else
+	echo "touchwake: did not find valid touchwake mode: setting disabled"
+	touchwake=0
+	echo 0 > /data/local/devil/touchwake
+	echo 0 > /sys/devices/virtual/misc/touchwake/enabled
+fi
+
+# set touchwake timeout
+echo; echo "touchwake_timeout"
+if [ "$touchwake" -eq 1 ]; then
+   if [ -e "/data/local/devil/touchwake_timeout" ];then
+	touchwake_timeout=`cat /data/local/devil/touchwake_timeout`
+	if [ "$touchwake_timeout" -le 300 ] && [ "$vibrator" -ge 0 ];then
+    		echo "touchwake_timeout: found valid touchwake_timeout: <$touchwake_timeout>"
+    		echo $touchwake_timeout > /sys/devices/virtual/misc/touchwake/delay
+	else
+		echo "touchwake_timeout: did not find valid touchwake_timeout: setting 30 sec."
+		touchwake_timeout=30
+		echo $touchwake_timeout > /sys/devices/virtual/misc/touchwake/delay
+	fi
+    else
+	echo "touchwake_timeout: did not find valid touchwake_timeout: setting 30 sec."
+	touchwake_timeout=30
+	echo $touchwake_timeout > /sys/devices/virtual/misc/touchwake/delay
+	echo $touchwake_timeout > /data/local/devil/touchwake_timeout
+   fi
 fi
 
 
@@ -370,7 +411,7 @@ cat_msg_sysfile "panic: " /proc/sys/kernel/panic
 echo; echo "read_ahead_kb"
 #cat_msg_sysfile "default: " /sys/devices/virtual/bdi/default/read_ahead_kb
 #if $BB [[ "$readahead" -eq 64 || "$readahead" -eq 128 || "$readahead" -eq 256 || "$readahead" -eq 512  || "$readahead" -eq 1024 || "$readahead" -eq 2048 || "$readahead" -eq 3096 ]];then
-#    echo "CPU: found vaild readahead: <$readahead>"
+#    echo "CPU: found valid readahead: <$readahead>"
 #else
     readahead=256
 #fi
@@ -384,10 +425,34 @@ echo 16 > /sys/block/mtdblock2/queue/read_ahead_kb # system
 echo 16 > /sys/block/mtdblock3/queue/read_ahead_kb # cache
 echo 64 > /sys/block/mtdblock6/queue/read_ahead_kb # datadata
 
-echo; echo "$(date) io"
+echo; echo "$(date) io scheduler"
 MTD=`$BB ls -d /sys/block/mtdblock*`
 LOOP=`$BB ls -d /sys/block/loop*`
 MMC=`$BB ls -d /sys/block/mmc*`
+
+# set IO scheduler
+if [ -e "/data/local/devil/iosched" ];then
+	iosched=`cat /data/local/devil/iosched`
+	if $BB [ "$iosched" == "noop" ] || $BB [ "$iosched" == "deadline" ] || $BB [ "$iosched" == "cfq" ] || $BB [ "$iosched" == "sio" ] || $BB [ "$iosched" == "vr" ] || $BB [ "$iosched" == "fiops" ]; then
+    		echo "iosched: found valid io scheduler: <$iosched>";
+	else
+    		echo "iosched: did not find valid io scheduler: setting sio";
+  		iosched=sio
+	fi
+else
+    	echo "iosched: did not find valid io scheduler: setting sio";
+      	iosched=sio
+    	echo $iosched > /data/local/devil/iosched
+fi
+
+
+
+# general IO tweaks
+for i in $MTD $MMC $LOOP;do
+    echo "$iosched" > $i/queue/scheduler
+    echo 0 > $i/queue/rotational
+    echo 0 > $i/queue/iostats
+done
       
 
 # mtd/mmc only tweaks
@@ -395,6 +460,7 @@ for i in $MTD $MMC;do
     echo 1024 > $i/queue/nr_requests
 done
 
+echo;
 for i in $MTD $MMC $LOOP $RAM;do
     cat_msg_sysfile "$i/queue/scheduler: " $i/queue/scheduler
     cat_msg_sysfile "$i/queue/rotational: " $i/queue/rotational
@@ -419,10 +485,10 @@ echo; echo "bus_limit"
 	bus_limit=`cat /data/local/devil/bus_limit`
 	echo "profile: found: <$bus_limit>";
 		if [ "$bus_limit" -eq 1 ]; then
-    			echo "bus_limit: found vaild bus_limit profile: <automatic>";
+    			echo "bus_limit: found valid bus_limit profile: <automatic>";
       			echo 1 > /sys/class/misc/devil_idle/bus_limit;
 		elif [ "$bus_limit" -eq 2 ]; then
-    			echo "bus_limit: found vaild bus_limit profile: <permanent>";
+    			echo "bus_limit: found valid bus_limit profile: <permanent>";
       			echo 2 > /sys/class/misc/devil_idle/bus_limit;
 		else
     			echo "bus_limit: setting bus_limit profile: <disabled>";
@@ -441,14 +507,14 @@ echo; echo "vibrator"
 if [ -e "/data/local/devil/vibrator" ];then
 	vibrator=`cat /data/local/devil/vibrator`
 	if [ "$vibrator" -le 43640 ] && [ "$vibrator" -ge 20000 ];then
-    		echo "vibrator: found vaild vibrator intensity: <$vibrator>"
+    		echo "vibrator: found valid vibrator intensity: <$vibrator>"
     		echo $vibrator > /sys/class/timed_output/vibrator/duty
 	else
-		echo "vibrator: did not find vaild vibrator intensity: setting default"
+		echo "vibrator: did not find valid vibrator intensity: setting default"
 		echo 40140 > /sys/class/timed_output/vibrator/duty
 	fi
 else
-	echo "vibrator: did not find vaild vibrator intensity: setting default"
+	echo "vibrator: did not find valid vibrator intensity: setting default"
 	echo 40140 > /data/local/devil/vibrator
 	echo 40140 > /sys/class/timed_output/vibrator/duty
 fi
@@ -458,14 +524,14 @@ echo; echo "wifi"
 if [ -e "/data/local/devil/wifi" ];then
 	wifi=`cat /data/local/devil/wifi`
 	if [ "$wifi" -eq 0 ] || [ "$wifi" -eq 1 ];then
-    		echo "wifi: found vaild wifi mode: <$wifi>"
+    		echo "wifi: found valid wifi mode: <$wifi>"
     		echo $wifi > /sys/module/bcmdhd/parameters/uiFastWifi
 	else
-		echo "wifi: did not find vaild wifi mode: setting default"
+		echo "wifi: did not find valid wifi mode: setting default"
 		echo 0 > /sys/module/bcmdhd/parameters/uiFastWifi
 	fi
 else
-	echo "wifi: did not find vaild wifi mode: setting default"
+	echo "wifi: did not find valid wifi mode: setting default"
 	echo 0 > /data/local/devil/wifi
 	echo 0 > /sys/module/bcmdhd/parameters/uiFastWifi
 fi
@@ -478,7 +544,7 @@ if [ -e "/data/local/devil/smooth_ui" ];then
     		echo $smooth_ui > /sys/class/misc/devil_tweaks/smooth_ui_enabled
     		echo "smooth_ui: $smooth_ui"
 	else
-    		echo "did not find vaild smooth_ui value: setting default (enabled)"
+    		echo "did not find valid smooth_ui value: setting default (enabled)"
     		echo 1 > /sys/class/misc/devil_tweaks/smooth_ui_enabled
 	fi
 else
@@ -496,7 +562,7 @@ if [ -e "/data/local/devil/governor" ];then
     		echo $governor > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
     		echo "governor set to: $governor"
 	else
-    		echo "did not find vaild governor: doing nothing"
+    		echo "did not find valid governor: doing nothing"
     		governor=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor`
     		echo "your current governor is: $governor"	
 	fi
@@ -580,7 +646,7 @@ echo; echo "swappiness"
 if [ -e "/data/local/devil/swappiness" ];then
 	swappiness=`cat /data/local/devil/swappiness`
 	if [ "$swappiness" -le 100 ] && [ "$swappiness" -ge 0 ];then
-    		echo "swappiness: found vaild swappiness value: <$swappiness>"
+    		echo "swappiness: found valid swappiness value: <$swappiness>"
     		echo $swappiness > /proc/sys/vm/swappiness
 	else
 		echo "swappiness: value has to be betwenn 0 and 100"
