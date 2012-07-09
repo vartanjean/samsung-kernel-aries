886ee98a98a945f19ea6329d25a254a693b20c57
Dominik Baronelli <dominik.baronelli@gmail.com>
2012-07-09 23:57:45 +0200

    using old wifi driver again

--- init/version.c
+++ init/version.c
@@ -44,5 +44,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
- " (Devil3_0.99_wifi1_i9000_CFS_BLN_VC)"
+ " (Devil3_0.99_wifi2_i9000_CFS_BLN_VC)"
 	" (" LINUX_COMPILER ") %s\n";
