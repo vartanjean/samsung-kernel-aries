f940e60f9f87f9235b143025e587e341b63948cf
DerTeufel <dominik-kassel@gmx.de>
2012-09-03 21:57:19 +0200

    Revert "idle2: v0.380"
    This reverts commit 763b899d3fe049511337593fc886202e15b6ce0f.

--- arch/arm/mach-s5pv210/idle2.c
+++ arch/arm/mach-s5pv210/idle2.c
@@ -35,6 +35,7 @@
 #include <linux/interrupt.h>
 #include <mach/pm-core.h>
 #include <mach/idle2.h>
+#include <linux/hrtimer.h>
 #include <linux/suspend.h>
 #include <linux/workqueue.h>
 
@@ -46,6 +47,7 @@
 #define IDLE2_FREQ		(800 * 1000) /* 800MHz Screen off ( L4) / Suspend */
 #define DISABLE_FURTHER_CPUFREQ	0x10
 #define ENABLE_FURTHER_CPUFREQ	0x20
+#define STATE_C1	1
 #define STATE_C2	2
 #define STATE_C3	3
 #define MAX_CHK_DEV		5
@@ -57,7 +59,7 @@ extern bool pllbus_changing;
 
 /* IDLE2 control flags */
 static u16 idle2_flags;
-#define IDLE2_DISABLED		(1 << 0)
+#define DISABLED_BY_SUSPEND	(1 << 0)
 #define EXTERNAL_ACTIVE		(1 << 1)
 #define WORK_INITIALISED	(1 << 2)
 #define INACTIVE_PENDING	(1 << 3)
@@ -69,15 +71,22 @@ static u16 idle2_flags;
 #define BLUETOOTH_REQUEST	(1 << 9)
 #define UART_TIMEOUT		(1 << 10)
 #define UART_REQUEST		(1 << 11)
-#define TOP_BLOCK_ON		(1 << 12)
+//#define AUDIO_ACTIVE		(1 << 12)
+//#define CPUFREQ_LOCK_ALLOWED	(1 << 13)
+//#define CPUFREQ_LOCK_ACTIVE	(1 << 14)
 
-
-#ifdef CONFIG_S5P_IDLE2_STATS
 /* For statistics */
-u32 done_C2, done_C3, fallthrough;
+/*
+ * FIXME: Use array or dump them altogether once it's confirmed working properly.
+ * I prefer dumping them, because they probably aren't necessary.
+ */
+u32 bail_C2, bail_C3;
+u32 request_C2, request_C3, enter_C2, enter_C3, done_C2, done_C3, done_C1;
 u8 idle_state;
+u32 idle_time;
 u64 time_in_state[3];
-#endif
+u32 bail_vic, bail_mmc, bail_gating, bail_i2s, bail_rtc, bail_nand, bail_C3_gating, bail_DMA;
+
 /* Work function declarations */
 static struct workqueue_struct *idle2_wq;
 struct work_struct idle2_external_active_work;
@@ -86,13 +95,13 @@ struct work_struct idle2_enable_topon_work;
 struct delayed_work idle2_cancel_topon_work;
 struct delayed_work idle2_lock_cpufreq_work;
 struct delayed_work idle2_unlock_cpufreq_work;
-
-static bool idle2_disabled __read_mostly;
+bool top_enabled __read_mostly;
 
 /*
  * For saving & restoring state
  */
 static unsigned long vic_regs[4];
+static unsigned long tmp, val;
 
 #define GPIO_OFFSET		0x20
 #define GPIO_CON_PDN_OFFSET	0x10
@@ -132,12 +141,25 @@ struct check_device_op chk_dev_op[] = {
 #define S3C_HSMMC_DATA_INHIBIT	0x00000002
 #define S3C_HSMMC_CLOCK_CARD_EN	0x0004
 
+extern void i2sdma_getpos(dma_addr_t *src);
+extern unsigned int get_rtc_cnt(void);
+
 inline static bool idle2_pre_entry_check(void)
 {
 	unsigned int reg1, reg2;
-	unsigned long tmp;
 	unsigned char iter;
+	dma_addr_t src;
+	unsigned int current_cnt;
 	void __iomem *base_addr;
+
+	/*
+	 * Check for RTC interrupt
+	 */
+	current_cnt = get_rtc_cnt();
+	if (unlikely(current_cnt < 0x40)) {
+		bail_rtc++;
+		return true;
+	}
 
 	/*
 	 * Check for HSMMC activity
@@ -155,6 +177,8 @@ inline static bool idle2_pre_entry_check(void)
 
 		if (unlikely((reg1 & (S3C_HSMMC_CMD_INHIBIT | S3C_HSMMC_DATA_INHIBIT))
 				|| (reg2 & (S3C_HSMMC_CLOCK_CARD_EN)))) {
+			/* Increment bail counter for stats */
+			bail_mmc++;
 			return true;
 		}
 	}
@@ -163,6 +187,7 @@ inline static bool idle2_pre_entry_check(void)
 	 * Check for pending VIC interrupt
 	 */
 	if (unlikely(__raw_readl(VA_VIC2 + VIC_RAW_STATUS) & vic_regs[2])) {
+		bail_vic++;
 		return true;
 	}
 
@@ -171,25 +196,40 @@ inline static bool idle2_pre_entry_check(void)
 	 */
 	base_addr = chk_dev_op[3].base;
 
-	tmp = __raw_readl(base_addr + 0x0000010c);
+	val = __raw_readl(base_addr + 0x0000010c);
 
-	if (unlikely(tmp & 0x1)) {
+	if (unlikely(val & 0x1)) {
+		bail_nand++;
+		return true;
+	}
+
+	/*
+	 * Check I2S DMA activity
+	 */
+	i2sdma_getpos(&src);
+	src = src & 0x3FFF;
+	src = 0x4000 - src;
+	if (unlikely(src < 0x150)) {
+		bail_i2s++;
 		return true;
 	}
 
 	/* check clock gating */
-	tmp = __raw_readl(S5P_CLKGATE_IP0);
-	if (unlikely(tmp & (S5P_CLKGATE_IP0_MDMA | S5P_CLKGATE_IP0_PDMA0
+	val = __raw_readl(S5P_CLKGATE_IP0);
+	if (unlikely(val & (S5P_CLKGATE_IP0_MDMA | S5P_CLKGATE_IP0_PDMA0
 					| S5P_CLKGATE_IP0_PDMA1))) {
+		bail_DMA++;
 		return true;
 	}
-	tmp = __raw_readl(S5P_CLKGATE_IP3);
-		if (unlikely(tmp & (S5P_CLKGATE_IP3_I2C0 | S5P_CLKGATE_IP3_I2C_HDMI_DDC
+	val = __raw_readl(S5P_CLKGATE_IP3);
+		if (unlikely(val & (S5P_CLKGATE_IP3_I2C0 | S5P_CLKGATE_IP3_I2C_HDMI_DDC
 						| S5P_CLKGATE_IP3_I2C2))) {
+		bail_gating++;
 		return true;
 	}
-	tmp = __raw_readl(S5P_CLKGATE_IP1);
-	if (tmp & S5P_CLKGATE_IP1_USBHOST) {
+	val = __raw_readl(S5P_CLKGATE_IP1);
+	if (val & S5P_CLKGATE_IP1_USBHOST) {
+		bail_gating++;
 		return true;
 	}
 
@@ -199,9 +239,9 @@ inline static bool idle2_pre_entry_check(void)
 
 inline static bool check_C3_clock_gating(void)
 {
-	unsigned long tmp;
-	tmp = __raw_readl(S5P_CLKGATE_IP0);
-	if (unlikely(tmp & S5P_CLKGATE_IP0_G3D)) {
+	val = __raw_readl(S5P_CLKGATE_IP0);
+	if (unlikely(val & S5P_CLKGATE_IP0_G3D)) {
+		bail_C3_gating++;
 		return true;
 	}
 	return false;
@@ -211,10 +251,8 @@ inline static bool check_C3_clock_gating(void)
  * Function which actually enters into DEEP-IDLE states
  */
 extern void s5p_enter_idle(void);
-inline static int s5p_enter_idle2(void)
+inline static int s5p_enter_idle2(bool top_enabled)
 {
-	unsigned long tmp;
-
 	if (unlikely(pm_cpu_sleep == NULL)) {
 		pr_err("%s: error: no cpu sleep function\n", __func__);
 		return -EINVAL;
@@ -233,7 +271,7 @@ inline static int s5p_enter_idle2(void)
 		return -EBUSY;
 
 	/* GPIO Power Down Control */
-	if (!(idle2_flags & TOP_BLOCK_ON)) {
+	if (likely(!top_enabled)) {
 		void __iomem *gpio_base = S5PV210_GPA0_BASE;
 		do {
 			/* Keep the previous state in idle2 mode */
@@ -249,60 +287,52 @@ inline static int s5p_enter_idle2(void)
 
 	/*
 	 * Configure IDLE_CFG register
+	 * Set ARM_L2_CACHE field to 0b111111
+	 *
+	 * For TOP block on:
+	 * TOP_LOGIC = ON
+	 * TOP_MEMORY = ON
+	 * ARM_L2_CACHE = Retention
+	 * CFG_DIDLE = DEEP
+	 *
+	 * For TOP block off
+	 * TOP_LOGIC = OFF
+	 * TOP_MEMORY = OFF
+	 * ARM_L2_CACHE = Retention
+	 * CFG_DIDLE = DEEP
 	 */
 	tmp = __raw_readl(S5P_IDLE_CFG);
-	/*
-	 * Clear bits 26 to 31
-	 * This unsets the TL, TM & L2 bits
-	 */
-	tmp &= ~(0x3f << 26);
-	if (!(idle2_flags & TOP_BLOCK_ON))
-		/*
-		 * TOP block OFF configuration
-		 * TOP logic retention, TOP memory retention
-		 * L2 cache OFF (bit 26 is already unset)
-		 */
-		tmp |= (S5P_IDLE_CFG_TL_RET | S5P_IDLE_CFG_TM_RET
-			| S5P_IDLE_CFG_DIDLE);
+	tmp &= ~(0x3fU << 26);
+	if (unlikely(top_enabled))
+		tmp |= (S5P_IDLE_CFG_TL_ON | S5P_IDLE_CFG_TM_ON
+			| S5P_IDLE_CFG_L2_RET | S5P_IDLE_CFG_DIDLE);
 	else
-		/*
-		 * TOP block ON configuration
-		 * TOP logic ON, TOP memory retention
-		 * L2 cache OFF (bit 26 is already unset)
-		 */
-		tmp |= (S5P_IDLE_CFG_TL_ON | S5P_IDLE_CFG_TM_RET
-			| S5P_IDLE_CFG_DIDLE);
+		tmp |= (S5P_IDLE_CFG_TL_RET | S5P_IDLE_CFG_TM_RET
+			| S5P_IDLE_CFG_L2_RET | S5P_IDLE_CFG_DIDLE);
 	__raw_writel(tmp, S5P_IDLE_CFG);
 
 	/*
-	 * Set configuration for idle2 entry
+	 * Setting configuration for idle2 entry...
 	 */
 
 	/*
 	 * Wakeup Source configuration:
-	 * Reset all unreserved bits to enable
-	 * all sources.
+	 * Set all bits, then reset required sources.
+	 * Enable RTC_ALARM, RTC_TICK, KEY, I2S, ST
 	 */
-	tmp = __raw_readl(S5P_WAKEUP_MASK);
-	tmp &= ~0xffff;
+	tmp |= 0xffffffffU;
+	tmp &= ~((1 << 1) | (1 << 2) | (1 << 5)
+		| (1 << 13) | (1 << 14));
 	__raw_writel(tmp, S5P_WAKEUP_MASK);
 
 	/*
 	 * External Interrupt wakeup sources:
-	 * Enable all sources, then mask sources
-	 * which cause spurious wakeups.
-	 *
-	 * EINT 1,2 & 30 are permanently pending
-	 * causing continual wakeups.
-	 *
-	 * We also don't reset the wakeup mask
-	 * after we have set it here, as these
-	 * spurious wakeups will also affect
-	 * C1 residencies so it's best to keep
-	 * these masked.
+	 * Firstly set all bits to avoid spurious wakeups
+	 * Enable max8998, modem, NFC, bcm4329, fsa9480, k3g
 	 */
-	tmp &= ~0xffffffff;
-	tmp |= ((1 << 1) | (1 << 2) | (1 << 30));
+	tmp |= 0xffffffffU;
+	tmp &= ~((1 << 7) | (1 << 11) | (1 << 12) | (1 << 15)
+		| (1 << 20) | (1 << 21) | (1 << 23) | (1 << 29));
 	__raw_writel(tmp, S5P_EINT_WAKEUP_MASK);
 
 	/*
@@ -322,8 +352,7 @@ inline static int s5p_enter_idle2(void)
 	__raw_writel(tmp, S5P_OTHERS);
 
 	/*
-	 * Clear wakeup status register by writing the current
-	 * value back to it.
+	 * Clear wakeup status register
 	 */
 	__raw_writel(__raw_readl(S5P_WAKEUP_STAT), S5P_WAKEUP_STAT);
 
@@ -332,9 +361,15 @@ inline static int s5p_enter_idle2(void)
 	 */
 
 	/*
+	 * Flush all caches before calling
+	 * s3c_idle2_cpu_save()
+	 */
+	flush_cache_all();
+
+	/*
 	 * Save CPU state and enter idle2 state
 	 */
-	s3c_cpu_save(0, PLAT_PHYS_OFFSET - PAGE_OFFSET);
+	s3c_idle2_cpu_save(0, PLAT_PHYS_OFFSET - PAGE_OFFSET);
 
 	/*
 	 * We have resumed from IDLE and returned.
@@ -342,7 +377,7 @@ inline static int s5p_enter_idle2(void)
 	 */
 	cpu_init();
 
-	if (!(idle2_flags & TOP_BLOCK_ON)) {
+	if (likely(!top_enabled)) {
 		/*
 		 * Release retention of GPIO/MMC/UART IO pads
 		 */
@@ -353,7 +388,14 @@ inline static int s5p_enter_idle2(void)
 	}
 
 	/*
-	 * Reset IDLE_CFG & PWR_CFG register
+	 * EINT Wakeup sources:
+	 * Reset all bits as per default
+	 */
+	tmp &= ~0xffffffffU;
+	__raw_writel(tmp, S5P_EINT_WAKEUP_MASK);
+
+	/*
+	 * Restore S5P_IDLE_CFG & S5P_PWR_CFG registers
 	 */
 	tmp = __raw_readl(S5P_IDLE_CFG);
 	tmp &= ~(S5P_IDLE_CFG_TL_MASK | S5P_IDLE_CFG_TM_MASK
@@ -377,9 +419,8 @@ inline int s5p_enter_idle_deep(struct cpuidle_device *device,
 				struct cpuidle_state *state)
 {
 	struct timeval before, after;
-	s8 err;
-	u8 *state_name = (state->name);
-	u32 idle_time;
+	char err;
+	unsigned char *state_name = (state->name);
 
 	/*
 	 * Convert second ascii character of state_name,
@@ -389,6 +430,11 @@ inline int s5p_enter_idle_deep(struct cpuidle_device *device,
 	unsigned char requested_state = (state_name[1] - 48);
 
 	do_gettimeofday(&before);
+
+	/*
+	 * Check for any soft-expired timers and run them
+	 */
+	hrtimer_peek_ahead_timers();
 
 	local_irq_disable();
 	local_fiq_disable();
@@ -411,92 +457,89 @@ inline int s5p_enter_idle_deep(struct cpuidle_device *device,
 
 	switch (requested_state){
 	case STATE_C2:
-		if (unlikely(idle2_pre_entry_check())) {
-			device->last_state = &device->states[0];
-#ifdef CONFIG_S5P_IDLE2_STATS
-			fallthrough++;
-			idle_state = 0;
-#endif
+		request_C2++;
+		if (unlikely(idle2_pre_entry_check()))
+			/* Fallthrough */
 			break;
-		} else {
-			idle2_flags |= TOP_BLOCK_ON;
-			err = s5p_enter_idle2();
-			if (unlikely(err)) {
-				device->last_state = &device->states[0];
-#ifdef CONFIG_S5P_IDLE2_STATS
-				fallthrough++;
-				idle_state = 0;
-#endif
-				break;
-			} else {
-#ifdef CONFIG_S5P_IDLE2_STATS
-				done_C2++;
-				idle2_update_wakeup_stats();
-				idle_state = 1;
-#endif
-				break;
-			}
-		}
+		else
+			goto enter_C2_state;
 	case STATE_C3:
-		if (unlikely(idle2_pre_entry_check())) {
-			device->last_state = &device->states[0];
-#ifdef CONFIG_S5P_IDLE2_STATS
-			fallthrough++;
-			idle_state = 0;
-#endif
+		request_C3++;
+		if (unlikely(idle2_pre_entry_check()))
+			/* Fallthrough */
 			break;
-		} else if (unlikely(check_C3_clock_gating())) {
+		else if (unlikely(check_C3_clock_gating())) {
 			/*
 			 * Tell cpuidle governor that we went into
 			 * C2 instead of C3
 			 */
 			device->last_state = &device->states[1];
-			idle2_flags |= TOP_BLOCK_ON;
-			err = s5p_enter_idle2();
-			if (unlikely(err)) {
-				device->last_state = &device->states[0];
-#ifdef CONFIG_S5P_IDLE2_STATS
-				fallthrough++;
-				idle_state = 0;
-#endif
-				break;
-			} else {
-#ifdef CONFIG_S5P_IDLE2_STATS
-				done_C2++;
-				idle2_update_wakeup_stats();
-				idle_state = 1;
-#endif
-				break;
-			}
-		} else {
-			idle2_flags &= ~TOP_BLOCK_ON;
-			err = s5p_enter_idle2();
-			if (unlikely(err)) {
-				device->last_state = &device->states[0];
-#ifdef CONFIG_S5P_IDLE2_STATS
-				fallthrough++;
-				idle_state = 0;
-#endif
-				break;
-			} else {
-#ifdef CONFIG_S5P_IDLE2_STATS
-				done_C3++;
-				idle2_update_wakeup_stats();
-				idle_state = 2;
-#endif
-				break;
-			}
-		}
+			goto enter_C2_state;
+		} else
+			goto enter_C3_state;
 	default:
-		WARN_ON(0);
-		device->last_state = &device->states[0];
-#ifdef CONFIG_S5P_IDLE2_STATS
-		fallthrough++;
-		idle_state = 0;
-#endif
+		/* Fallthrough */
 		break;
 	}
 
+	/*
+	 * If we fall through enter C1.
+	 * WARNING: WE *must* restore VIC interupts to enter C1.
+	 *
+	 * When we return from C1, tell the cpuidle governor we
+	 * ended up in C1.
+	 * We don't want to be penalised for short residencies
+	 * which would harm the chances of C2 or C3 being
+	 * selected next time.
+	 */
+	__raw_writel(vic_regs[0], VA_VIC0 + VIC_INT_ENABLE);
+	__raw_writel(vic_regs[1], VA_VIC1 + VIC_INT_ENABLE);
+	__raw_writel(vic_regs[2], VA_VIC2 + VIC_INT_ENABLE);
+	__raw_writel(vic_regs[3], VA_VIC3 + VIC_INT_ENABLE);
+	s5p_enter_idle();
+	done_C1++;
+	idle_state = 0;
+	device->last_state = &device->states[0];
+	goto restore_interrupts;
+
+enter_C2_state:
+	enter_C2++;
+	err = s5p_enter_idle2(true);
+	/*
+	 * To keep C2 stats accurate and to ensure that the idle
+	 * governor doesn't start penalising us for residency times
+	 * below target_residency + exit_latency, lie to cpuidle
+	 * and tell it we were in C1 if return from C2 with an error.
+	 */
+	if (unlikely(err)) {
+		device->last_state = &device->states[0];
+		pr_debug("idle2: C2 exited with error %d\n", err);
+		bail_C2++;
+	} else {
+		done_C2++;
+		idle_state = 1;
+	}
+	goto restore_vic_state;
+
+enter_C3_state:
+	enter_C3++;
+	err = s5p_enter_idle2(false);
+	/*
+	 * To keep C3 stats accurate and to ensure that the idle
+	 * governor doesn't start penalising us for residency times
+	 * below target_residency + exit_latency, lie to cpuidle
+	 * and tell it we were in C1 if return from C3 with an error.
+	 */
+	if (unlikely(err)) {
+		device->last_state = &device->states[0];
+		pr_debug("idle2: C3 exited with error %d\n", err);
+		bail_C3++;
+	} else {
+		done_C3++;
+		idle_state = 2;
+	}
+
+restore_vic_state:
 	/*
 	 * Restore saved VIC registers and enable interrupts
 	 */
@@ -505,18 +548,24 @@ inline int s5p_enter_idle_deep(struct cpuidle_device *device,
 	__raw_writel(vic_regs[2], VA_VIC2 + VIC_INT_ENABLE);
 	__raw_writel(vic_regs[3], VA_VIC3 + VIC_INT_ENABLE);
 
-	do_gettimeofday(&after);
+/*
+ * We put this here because we don't want wakeup reasons if
+ * we fall back to C1, because it makes the stats meaningless.
+ */
+#ifdef CONFIG_S5P_IDLE2_STATS
+	idle2_update_wakeup_stats();
+#endif
 
+restore_interrupts:
+	do_gettimeofday(&after);
 	local_fiq_enable();
 	local_irq_enable();
 
 	idle_time = (after.tv_sec - before.tv_sec) * USEC_PER_SEC +
 			(after.tv_usec - before.tv_usec);
-#ifdef CONFIG_S5P_IDLE2_STATS
-	if (likely(idle_state))
-		time_in_state[idle_state] += (u64)idle_time;
+	time_in_state[idle_state] += (u64)idle_time;
+
 	idle_state = 0;
-#endif
 	return idle_time;
 }
 
@@ -533,8 +582,8 @@ static void idle2_lock_cpufreq_work_fn(struct work_struct *work)
 #else
 	cpufreq_driver_target(cpufreq_cpu_get(0), IDLE2_FREQ,
 			DISABLE_FURTHER_CPUFREQ);
-	pr_info("%s: CPUfreq locked to %dKHz\n", __func__, IDLE2_FREQ);
 #endif
+//	idle2_flags |= CPUFREQ_LOCK_ACTIVE;
 }
 
 static void idle2_unlock_cpufreq_work_fn(struct work_struct *work)
@@ -549,19 +598,39 @@ static void idle2_unlock_cpufreq_work_fn(struct work_struct *work)
 			ENABLE_FURTHER_CPUFREQ);
 	pr_info("%s: CPUfreq unlocked from %dKHz\n", __func__, IDLE2_FREQ);
 #endif
+//	idle2_flags &= ~CPUFREQ_LOCK_ACTIVE;
 }
+
+// void idle2_cpufreq_lock(bool flag)
+// {
+// 	if (idle2_flags & EARLYSUSPEND_ACTIVE) {
+// 		if (flag && (idle2_flags & AUDIO_ACTIVE))
+// 			queue_delayed_work(idle2_wq, &idle2_lock_cpufreq_work, 0);
+// 	} else if (!flag && (idle2_flags & CPUFREQ_LOCK_ACTIVE))
+// 			queue_delayed_work(idle2_wq, &idle2_unlock_cpufreq_work, 0);
+// }
+
+// void idle2_audio_active(bool flag)
+// {
+// 	if (flag) {
+// 		idle2_flags |= AUDIO_ACTIVE;
+// 		idle2_cpufreq_lock(true);
+// 	} else {
+// 		idle2_flags &= ~AUDIO_ACTIVE;
+// 		idle2_cpufreq_lock(false);
+// 	}
+// }
 
 void earlysuspend_active_fn(bool flag)
 {
 	if (flag) {
 		idle2_flags |= EARLYSUSPEND_ACTIVE;
 		queue_delayed_work(idle2_wq, &idle2_lock_cpufreq_work, 0);
-		pr_info("idle2: |= EARLYSUSPEND_ACTIVE\n");
 	} else {
 		idle2_flags &= ~EARLYSUSPEND_ACTIVE;
 		queue_delayed_work(idle2_wq, &idle2_unlock_cpufreq_work, 0);
-		pr_info("idle2: &= ~EARLYSUSPEND_ACTIVE\n");
 	}
+	pr_info("idle2: earlysuspend_active: %d\n", flag);
 }
 
 static void idle2_external_active_work_fn(struct work_struct *work)
@@ -697,12 +766,12 @@ static int idle2_pm_notify(struct notifier_block *nb,
 	unsigned long event, void *dummy)
 {
 	if (event == PM_SUSPEND_PREPARE) {
-		disable_hlt();
-		pr_info("%s: disable_hlt()\n", __func__);
+		idle2_flags |= DISABLED_BY_SUSPEND;
+		pr_info("%s: IDLE2 disabled\n", __func__);
 	}
 	else if (event == PM_POST_SUSPEND) {
-		enable_hlt();
-		pr_info("%s: enable_hlt()\n", __func__);
+		idle2_flags &= ~DISABLED_BY_SUSPEND;
+		pr_info("%s: IDLE2 enabled\n", __func__);
 	}
 	return NOTIFY_OK;
 }
@@ -717,15 +786,14 @@ static struct notifier_block idle2_pm_notifier = {
 
 inline int s5p_idle_prepare(struct cpuidle_device *device)
 {
-	if (unlikely(idle2_flags & IDLE2_DISABLED)) {
+	if (unlikely(idle2_flags & DISABLED_BY_SUSPEND)) {
 		/*
-		 * Ignore DEEP-IDLE states
+		 * Ignore both DEEP-IDLE states
 		 */
 		device->states[1].flags |= CPUIDLE_FLAG_IGNORE;
 		device->states[2].flags |= CPUIDLE_FLAG_IGNORE;
 		return 0;
 	}
-
 	if ((idle2_flags & NEEDS_TOPON)
 		|| !(idle2_flags & EARLYSUSPEND_ACTIVE)
 		|| (idle2_flags & EXTERNAL_ACTIVE)) {
@@ -794,28 +862,5 @@ int s5p_idle2_post_init(void)
 			return -EINVAL;
 		}
 	}
-
 	return 0;
 }
-
-static int idle2_disabled_set(const char *arg, const struct kernel_param *kp)
-{
-
-	param_set_bool(arg, kp);
-
-	if (idle2_disabled) {
-		idle2_flags |= IDLE2_DISABLED;
-		pr_info("%s: |= IDLE2_DISABLED\n", __func__);
-	} else {
-		idle2_flags &= ~IDLE2_DISABLED;
-		pr_info("%s: &= ~IDLE2_DISABLED\n", __func__);
-	}
-	return 0;
-}
-
-static struct kernel_param_ops idle2_disabled_ops = {
-	.set = idle2_disabled_set,
-	.get = param_get_bool,
-};
-module_param_cb(disable, &idle2_disabled_ops, &idle2_disabled, 0644);
-MODULE_PARM_DESC(disable, "Disable C2 & C3 states");
