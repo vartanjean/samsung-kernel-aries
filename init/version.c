c8ab07ef405d4860e10050767a5372061b1a3a4d
DerTeufel <dominik-kassel@gmx.de>
2012-09-06 00:07:08 +0200

    1.4.1_jellybean

--- init/version.c
+++ init/version.c
@@ -44,5 +44,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
- " (Devil3_1.4.1_Hellybean_fascinate_CFS_BLN_VC)"
+ " (Devil3_1.4.1_Jellybean_fascinate_CFS_BLN_VC)"
 	" (" LINUX_COMPILER ") %s\n";
