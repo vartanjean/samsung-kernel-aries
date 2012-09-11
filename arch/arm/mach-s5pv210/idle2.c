5e626df0eb3308cd6e12a33143286a626c8ab862
DerTeufel <dominik-kassel@gmx.de>
2012-09-11 16:50:01 +0200

    Revert "idle2: use fixed freq, inform live_oc correctly"
    This reverts commit 5c8a96b021105a71800a3e9b4d3e005dbfaec7aa.

--- arch/arm/mach-s5pv210/idle2.c
+++ arch/arm/mach-s5pv210/idle2.c
@@ -46,7 +46,6 @@
 
 #ifdef CONFIG_LIVE_OC
 extern unsigned long cpuL4freq(void); /* to replace hardcoded 800 mhz with live_oc'ed freq */
-extern bool pllbus_changing;
 #endif
 
 /* IDLE2 control flags */
@@ -585,7 +584,6 @@ static void idle2_lock_cpufreq_work_fn(struct work_struct *work)
 static void idle2_unlock_cpufreq_work_fn(struct work_struct *work)
 {
 #ifdef CONFIG_LIVE_OC
-	pllbus_changing = true;
 	cpufreq_driver_target(cpufreq_cpu_get(0), cpuL4freq(),
 			ENABLE_FURTHER_CPUFREQ);
 	pr_info("%s: CPUfreq unlocked from %dKHz\n", __func__, cpuL4freq());
