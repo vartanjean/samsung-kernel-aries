50bf5bea7bf6c76a304859b0689de69a35267643
DerTeufel <dominik-kassel@gmx.de>
2012-09-28 08:34:34 +0200

    1.6.0

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ export USE_CCACHE=1
 	CCACHE_LOGFILE="/home/dominik/android/ccache/ccache-log"
         export CCACHE_DIR CCACHE_COMPRESS CCACHE_LOGFILE
 ###########################################################################################################
-number="1.6.0_Jellybean"
+number="1.6.0_Jelly_Helly"
 
 target="$1"
 
