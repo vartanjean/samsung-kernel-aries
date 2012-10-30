0ba52d503380cd0fb3f01321ba79eb92b38078ff
DerTeufel <dominik-kassel@gmx.de>
2012-10-30 11:08:41 +0100

    revert: dynamic fsync from faux123

--- fs/sync.c
+++ fs/sync.c
@@ -18,10 +18,6 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-extern bool early_suspend_active;
-#endif
-
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
 
@@ -95,7 +91,7 @@ static void sync_one_sb(struct super_block *sb, void *arg)
  * Sync all the data for all the filesystems (called by sys_sync() and
  * emergency sync)
  */
-void sync_filesystems(int wait)
+static void sync_filesystems(int wait)
 {
 	iterate_supers(sync_one_sb, &wait);
 }
@@ -186,11 +182,6 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 	    return 0;
 #endif
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-  if (!early_suspend_active)
-    return 0;
-  else {
-#endif
 	if (!file->f_op || !file->f_op->fsync) {
 		ret = -EINVAL;
 		goto out;
@@ -210,9 +201,6 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 
 out:
 	return ret;
-#ifdef CONFIG_DYNAMIC_FSYNC
-  }
-#endif
 }
 EXPORT_SYMBOL(vfs_fsync_range);
 
@@ -256,17 +244,11 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-
 #ifdef CONFIG_FSYNC_CONTROL
 	if (!fsynccontrol_fsync_enabled())
 	    return 0;
 #endif
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (!early_suspend_active)
-		return 0;
-	else
-#endif
 	return do_fsync(fd, 0);
 }
 
@@ -277,11 +259,6 @@ SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 	    return 0;
 #endif
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (!early_suspend_active)
-		return 0;
-	else
-#endif
 	return do_fsync(fd, 1);
 }
 
@@ -357,12 +334,6 @@ EXPORT_SYMBOL(generic_write_sync);
 SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 				unsigned int flags)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (!early_suspend_active)
-		return 0;
-	else {
-#endif
-
 	int ret;
 	struct file *file;
 	struct address_space *mapping;
@@ -447,9 +418,6 @@ out_put:
 	fput_light(file, fput_needed);
 out:
 	return ret;
-#ifdef CONFIG_DYNAMIC_FSYNC
-	}
-#endif
 }
 #ifdef CONFIG_HAVE_SYSCALL_WRAPPERS
 asmlinkage long SyS_sync_file_range(long fd, loff_t offset, loff_t nbytes,
@@ -466,11 +434,6 @@ SYSCALL_ALIAS(sys_sync_file_range, SyS_sync_file_range);
 SYSCALL_DEFINE(sync_file_range2)(int fd, unsigned int flags,
 				 loff_t offset, loff_t nbytes)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (!early_suspend_active)
-		return 0;
-	else
-#endif
 	return sys_sync_file_range(fd, offset, nbytes, flags);
 }
 #ifdef CONFIG_HAVE_SYSCALL_WRAPPERS
